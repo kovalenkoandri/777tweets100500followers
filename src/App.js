6d2440b9e964c5610ecc11834a54dccb71188ebf
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-02 23:59:07 +0200

    container

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" className='ButtonFollow'></button>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <div className="container">
+          <div className="wrapperStatistics">
+            <span className="tweetsText">777 tweets</span>
+            <span className="followersText">100,500 followers</span>
+          </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <button type="button" className="ButtonFollow">
+            <span className="ButtonFollowSpan">Following</span>
+          </button>
+        </div>
       </header>
     </div>
   );
